4afe15131d445e966c9a00beed21b19f12993a0f
Ann Shcherbakova <animal_spirit@mail.ru>
2023-01-28 17:51:04 +0300

    commit

--- app/php/connection.php
+++
@@ -1,9 +0,0 @@
-
-$dblocation = "127.0.0.1:3306";
-$dbname = "u0958551_dmotor";
-$dbuser = "u0958551_dmuser";
-$dbpass = "7UpecnMMgBiVE36";
-
-$conn = mysqli_connect($dblocation, $dbuser, $dbpass, $dbname);
-
-mysqli_set_charset($conn, "utf8");
